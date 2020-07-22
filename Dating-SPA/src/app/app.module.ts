571e4286a2079ba31021e3b370512923b2e85efc
unknown <kavitha>
2020-07-22 14:52:55 +0530

    End of Sec 7

--- Dating-SPA/src/app/app.module.ts
+++ Dating-SPA/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,6 +15,10 @@ import { ErrorInterceptorProvider } from './_service/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { BsDropdownModule } from 'ngx-bootstrap';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { appRoutes } from './routes';
 
 @NgModule({
    declarations: [
@@ -19,14 +26,18 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
       ValueComponent,
       NavComponent,
       RegisterComponent,
-      HomeComponent
+      HomeComponent,
+      ListsComponent,
+      MessagesComponent,
+      MemberListComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
--- Dating-SPA/src/app/nav/nav.component.ts
+++ Dating-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
 import { AlertifyService } from '../_service/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +10,33 @@ import { AlertifyService } from '../_service/alertify.service';
 })
 export class NavComponent implements OnInit {
 model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
   }
 
+  // tslint:disable-next-line: typedef
   login() {
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Logged in Success');
     }, error => {
        this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
     console.log(this.model);
   }
 
+  // tslint:disable-next-line: typedef
   loggedIn() {
     return this.authService.loggedIn();
   }
-  
+
+  // tslint:disable-next-line: typedef
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
